f4f562f54aeb77460c4f2362f0aab75f68d135cf
Sandeep K <sandeepkumaresan4676@gmail.com>
2026-05-20 17:21:33 +0530

    project get

--- src/main/java/com/sandeep/portfolio/controller/ProjectController.java
+++ src/main/java/com/sandeep/portfolio/controller/ProjectController.java
@@ -1,7 +1,23 @@
 package com.sandeep.portfolio.controller;
 
+import com.sandeep.portfolio.dto.ProjectDTO;
+import com.sandeep.portfolio.entity.Project;
+import com.sandeep.portfolio.service.ProjectService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+import java.util.List;
+
+@RestController
 public class ProjectController {
+    private ProjectService projectService;
+    public ProjectController(ProjectService projectService){
+        this.projectService = projectService;
+    }
+
+    @GetMapping("/projects")
+    public List<ProjectDTO> getProjects(){
+        return projectService.getProjects();
+    }
 }
--- src/main/java/com/sandeep/portfolio/service/ProjectService.java
+++ src/main/java/com/sandeep/portfolio/service/ProjectService.java
@@ -1,7 +1,34 @@
 package com.sandeep.portfolio.service;
 
+import com.sandeep.portfolio.dto.ProjectDTO;
+import com.sandeep.portfolio.entity.Project;
+import com.sandeep.portfolio.repository.Projectrepo;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class ProjectService {
+    private Projectrepo projectrepo;
+    public ProjectService(Projectrepo projectrepo) {
+        this.projectrepo = projectrepo;
+    }
+
+    public List<ProjectDTO> getProjects(){
+
+        List<ProjectDTO> dtos = new ArrayList<>();
+        List<Project> project = projectrepo.findAll();
+        for(Project p : project) {
+            ProjectDTO dto = new ProjectDTO();
+            dto.setTitle(p.getTitle());
+            dto.setDescription(p.getDescription());
+            dto.setTech_stack(p.getTech_stack());
+            dto.setGithub_url(p.getGithub_url());
+            dtos.add(dto);
+        }
+        return dtos;
+    }
+
+
 }
