11205d02aeb039f6b797644f73a04294038e50f9
Sandeep K <sandeepkumaresan4676@gmail.com>
2026-05-21 01:16:03 +0530

    contact add

--- src/main/java/com/sandeep/portfolio/controller/ContactController.java
+++ src/main/java/com/sandeep/portfolio/controller/ContactController.java
@@ -1,7 +1,20 @@
 package com.sandeep.portfolio.controller;
 
+import com.sandeep.portfolio.dto.ContactDTO;
+import com.sandeep.portfolio.service.ContactService;
+import com.sandeep.portfolio.service.ProjectService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class ContactController {
+    private ContactService contactService;
+    public ContactController(ContactService contactService){
+        this.contactService = contactService;
+    }
+    @GetMapping("/contact")
+    public String addContact(ContactDTO contactDTO){
+        return contactService.addContact(contactDTO);
+    }
 }
--- src/main/java/com/sandeep/portfolio/service/ContactService.java
+++ src/main/java/com/sandeep/portfolio/service/ContactService.java
@@ -1,7 +1,23 @@
 package com.sandeep.portfolio.service;
 
+import com.sandeep.portfolio.dto.ContactDTO;
+import com.sandeep.portfolio.entity.Contact;
+import com.sandeep.portfolio.repository.Contactrepo;
 import org.springframework.stereotype.Service;
 
 @Service
 public class ContactService {
+    private Contactrepo contactrepo;
+    public ContactService(Contactrepo contactrepo) {
+        this.contactrepo = contactrepo;
+    }
+
+    public String addContact(ContactDTO contactDTO) {
+        Contact contact = new Contact();
+        contact.setName(contactDTO.getName());
+        contact.setEmail(contactDTO.getEmail());
+        contact.setMessage(contactDTO.getMessage());
+        contactrepo.save(contact);
+        return "Contact Added Successfully";
+    }
 }
